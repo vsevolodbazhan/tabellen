5ba52da34990f1125dc01d9e0de2710ecbdae033
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-26 14:19:46 +1000

    Add `client` module
    This module defines a `Client` class that described a client
    of a Tomoru bot.
    
    And it defines a set of function that allow to extract
    clients from a Google Spreadsheet.

---
+++ tabellen/client.py
@@ -0,0 +1,166 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+from .sheets import extract_values
+
+__all__ = ["Client", "extract_clients"]
+
+
+@dataclass
+class Client:
+    """A client of a Tomoru bot.
+
+    A client of a Tomoru bot described by a bot ID and a chat ID.
+
+    Attributes:
+        bot (str): A bot ID.
+        chat (int): A chat ID.
+
+    Examples:
+        >>> client = Client('abc123', 'cde456')
+        >>> client
+        Client(bot='abc123', chat='cde456')
+        >>> client.bot
+        'abc123'
+        >>> client.chat
+        'cde456'
+    """
+
+    bot: str
+    chat: str
+
+
+def read_between(value: str, prefix: str, postfix: str) -> Optional[str]:
+    """Read a sequence between given prefix and postfix.
+
+    Args:
+        value (str): A value to read a sequence from.
+        prefix (str): A prefix after which a sequence starts.
+        postfix (str): A postfix before which a sequence ends.
+
+    Returns:
+        str: The sequence.
+        None: If prefix or postfix were not found.
+
+    Examples:
+        >>> read_between(value='abc', prefix='a', postfix='c')
+        'b'
+        >>> read_between(value='abc', prefix='a', postfix='d')
+        >>> read_between(value='abc', prefix='z', postfix='c')
+    """
+
+    start = value.find(prefix)
+    if start == -1:
+        return None
+    start += len(prefix)
+
+    end = value.find(postfix)
+    if end == -1:
+        return None
+
+    return value[start:end]
+
+
+def retrieve_bot(value: str) -> str:
+    """Retrieve a bot ID from a string.
+
+    Retrieve a bot ID between 'bot/' and '/chats' from a string.
+
+    Args:
+        value (str): A string that a bot ID is retrieved from.
+
+    Returns:
+        str: The retrieved bot ID.
+
+    Raises:
+        ValueError: If bot ID could not be retrieved.
+
+    Examples:
+        >>> retrieve_bot('bot/abc/chats')
+        'abc'
+        >>> retrieve_bot('abc/chats')
+        Traceback (most recent call last):
+            ...
+        ValueError: Could not retrieve a bot ID from: abc/chats
+        >>> retrieve_bot('bot/abc')
+        Traceback (most recent call last):
+            ...
+        ValueError: Could not retrieve a bot ID from: bot/abc
+    """
+
+    prefix, postfix = "bot/", "/chats"
+    if bot := read_between(value=value, prefix=prefix, postfix=postfix):
+        return bot
+    raise ValueError(f"Could not retrieve a bot ID from: {value}")
+
+
+def retrieve_chat(value: str) -> str:
+    """Retrieve a chat ID from a string.
+
+    Retrieve a chat ID between 'chats/' and '";' from a string.
+
+    Args:
+        value (str): A string that a chat ID is retrieved from.
+
+    Returns:
+        str: The retrieved chat ID.
+
+    Raises:
+        ValueError: If chat ID could not be retrieved.
+
+    Examples:
+        >>> retrieve_chat('chats/abc";')
+        'abc'
+        >>> retrieve_bot('chats/abc')
+        Traceback (most recent call last):
+            ...
+        ValueError: Could not retrieve a bot ID from: chats/abc
+        >>> retrieve_bot('/abc";')
+        Traceback (most recent call last):
+            ...
+        ValueError: Could not retrieve a bot ID from: /abc";
+    """
+
+    prefix, postfix = "chats/", '";'
+    if chat := read_between(value=value, prefix=prefix, postfix=postfix):
+        return chat
+    raise ValueError(f"Could not retrieve a chat ID from: {value}")
+
+
+def extract_clients(
+    spreadsheet_id: str,
+    column: str,
+    range_start: int,
+    range_end: int,
+) -> List[Client]:
+    """
+    Extract clients from a spreadsheet.
+
+    Extract clients described by a bot ID and a chat ID from a spreadsheet.
+    Clients must be stored as hyperlinks.
+    For example: =HYPERLINK("https://app.tomoru.ru/bot/abc123/chats/cde456"; "John Doe")
+
+    Args:
+        spreadsheet_id (str): An ID of a Google Spreadsheet.
+        column (str): A spreadsheet column. For example, 'A' or 'AZ'.
+        range_start (int): An upper bound of the extraction range.
+        range_end (int): A lower bound of the extraction range.
+
+    Returns:
+        List[Client]: The list of extracted clients.
+    """
+
+    clients = []
+    values = extract_values(
+        spreadsheet_id=spreadsheet_id,
+        column=column,
+        range_start=range_start,
+        range_end=range_end,
+        render_option="FORMULA",
+    )
+
+    for value in values:
+        client = Client(bot=retrieve_bot(value), chat=retrieve_chat(value))
+        clients.append(client)
+
+    return clients
