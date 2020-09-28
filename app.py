206a0ade17732d476aaad4c3e9f13f55897dc152
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-28 20:32:08 +1000

    Implement generic 500 response

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+from http import HTTPStatus as status
+
 from tabellen import config
+
+
+def handle_server_error(error):
+    return "", status.INTERNAL_SERVER_ERROR
+
 
 connexion_app = config.connexion_app
 connexion_app.add_api("swagger.yml")
+connexion_app.add_error_handler(status.INTERNAL_SERVER_ERROR, handle_server_error)
 
 if __name__ == "__main__":
     connexion_app.run(debug=True)
