d2b81e37ec4cb634d879d17b17ffb5213f886909
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-28 20:20:55 +1000

    Implement `/send-message` endpoint

--- tabellen/api.py
+++ tabellen/api.py
@@ -1,11 +1,41 @@
-from connexion import NoContent
 from http import HTTPStatus
 
-from .hooks import decode_callback_url
+from connexion import NoContent
+
+from .clients import extract_clients
+from .event import Event
+from .hooks import Hook, decode_callback_url
 
 
 def hello():
     return {"message": "Hello!"}
+
+
+def send_message(body):
+    bot_id, spreadsheet_id, column, range_start, range_end = (
+        body["botId"],
+        body["config"]["spreadsheetId"],
+        body["config"]["column"],
+        body["config"]["rangeStart"],
+        body["config"]["rangeEnd"],
+    )
+
+    url = Hook.find_url_by_bot(bot=bot_id)
+    if url is None:
+        return NoContent, HTTPStatus.FAILED_DEPENDENCY
+
+    event_type = "newMessage"
+    clients = extract_clients(
+        spreadsheet_id=spreadsheet_id,
+        column=column,
+        range_start=range_start,
+        range_end=range_end,
+    )
+    for client in clients:
+        event = Event(_type=event_type, client=client)
+        event.send()
+
+    return NoContent, HTTPStatus.OK
 
 
 def subscribe(body):
