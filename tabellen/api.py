2cd9f613c8118bbd17651d9240d8730cc0788218
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-28 14:41:30 +1000

    Implement `/subscribe` endpoint

--- tabellen/api.py
+++ tabellen/api.py
@@ -1,2 +1,17 @@
+from connexion import NoContent
+from http import HTTPStatus
+
+from .hooks import decode_callback_url
+
+
 def hello():
     return {"message": "Hello!"}
+
+
+def subscribe(body):
+    tomoru_callback_url = body["tomoruCallbackUrl"]
+
+    hook = decode_callback_url(url=tomoru_callback_url)
+    hook.save()
+
+    return NoContent, HTTPStatus.OK
