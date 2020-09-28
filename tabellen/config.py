6df4be39c65b8e1f9f780c1237835bfd443e3dd0
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-28 21:11:46 +1000

    Get `MONGO_URI` value from environment

--- tabellen/config.py
+++ tabellen/config.py
@@ -1,12 +1,11 @@
+import os
+
 import connexion
 from flask_pymongo import PyMongo
-
-DATABASE_NAME = "tabellen"
-MONGO_URI = f"mongodb://localhost:27017/{DATABASE_NAME}"
 
 connexion_app = connexion.App(__name__, specification_dir="specs/")
 
 app = connexion_app.app
-app.config["MONGO_URI"] = MONGO_URI
+app.config["MONGO_URI"] = os.environ["MONGO_URI"]
 
 mongo = PyMongo(app)
