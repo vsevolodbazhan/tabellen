b13522b849e093d3923ca283ad623ca95ee1afc0
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-10-11 14:52:29 +1000

    Update tests for `Client`

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture
+def sheet_url():
+    return "https://docs.google.com/spreadsheets/d/1dhFwVyDeJ5z7Hg0MPtusvcqWAlPZF82lw5g0MaW6YdY/edit?usp=sharing"  # noqa: E501
--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,11 +1,9 @@
 from tabellen.clients import Client, extract_clients
 from tabellen.sheets import retrieve_id
 
-TEST_SHEET_URL = "https://docs.google.com/spreadsheets/d/1dhFwVyDeJ5z7Hg0MPtusvcqWAlPZF82lw5g0MaW6YdY/edit?usp=sharing"  # noqa: E501
 
-
-def test_extract_clients():
-    spreadsheet_id = retrieve_id(url=TEST_SHEET_URL)
+def test_extract_clients(sheet_url):
+    spreadsheet_id = retrieve_id(url=sheet_url)
     clients = extract_clients(
         spreadsheet_id=spreadsheet_id, column="A", range_start=2, range_end=3
     )
